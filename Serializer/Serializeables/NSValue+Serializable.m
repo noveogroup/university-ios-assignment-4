d0ddf56e10882e5b94038594425975995b542ba2
djachenko <i.s.djachenko@gmail.com>
2014-07-25 16:20:21 +0700

    Moved from using self.description

--- Serializer/Serializeables/NSValue+Serializable.m
+++ Serializer/Serializeables/NSValue+Serializable.m
@@ -4,12 +4,9 @@
 
 - (NSString *)serializeWithError:(NSError **)error
 {
-	NSString *description = [self description];
-	// CR: use this method to check internal value type: [self objCType].
-	// CR: you should not use "description" for anything but debug purposes.
-	if ([description rangeOfString:@"NSRect"].location != NSNotFound) {
-		// CR: Current style expects from you this: [self CGRectValue].
-		CGRect rect = self.CGRectValue;
+	if (strcmp([self objCType], @encode(CGRect)) == 0)
+	{
+		CGRect rect = [self CGRectValue];
 
 		return [NSString stringWithFormat:@"CGRect: [(%f, %f), (%f, %f)]", rect.origin.x, rect.origin.y, rect.size.width, rect.size.height];
 	}
