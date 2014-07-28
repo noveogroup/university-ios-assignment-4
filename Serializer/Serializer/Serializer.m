b377abe219945210853abcdcbeb8e89ce404db78
Alexey Ershov <alaershov@gmail.com>
2014-07-28 18:38:13 +0700

    Make Serializer helper generate more pretty indentations

--- Serializer/Serializer/Serializer.m
+++ Serializer/Serializer/Serializer.m
@@ -8,24 +8,33 @@
 
 #import "Serializer.h"
 #import "NSDictionary+Serializable.h"
+#import "SerializationError.h"
 
 @implementation Serializer
 
 + (NSString *) getIndentStringForDepth:(unsigned int)depth {
     NSMutableString *result = [[NSMutableString alloc] init];
     
-    for (unsigned int i = 0; i < depth; i++) {
-        [result appendString:@"|-"];
+    if (depth == 0) {
+        return @"";
     }
+    
+    for (unsigned int i = 0; i < depth - 1; i++) {
+        [result appendString:@"|  "];
+    }
+    
+    [result appendString:@"|--"];
     
     return result;
 }
 
 - (NSString *)serialize:(id)dictionary
-                  error:(NSError **)error {
-    if (![dictionary isMemberOfClass:[NSDictionary class]]) {
-        // TODO: create WrongClass error
-        
+                  error:(NSError *__autoreleasing *)error {
+    if (![dictionary isKindOfClass:[NSDictionary class]]) {
+        if (error != NULL) {
+            *error = [SerializationError getNotDictionaryErrorForClassName:[[dictionary class] description]];
+        }
+    
         return nil;
     }
     
