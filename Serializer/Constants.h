21a93798c2b0117f04eb633096c825a3b55fc892
djachenko <i.s.djachenko@gmail.com>
2014-07-29 11:57:41 +0700

    Constants moved to global scope from class

--- Serializer/Constants.h
+++ Serializer/Constants.h
@@ -1,9 +1,6 @@
 #import <Foundation/Foundation.h>
 
-
-@interface Constants : NSObject
 extern NSString *const myOwnDomain;
 extern int const unsupportedParameterType;
 extern int const unsupportedKeyType;
 extern NSString *const nullRepresentationString;
-@end
