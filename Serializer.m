34df07a6e958d54d029431503be830d9f87a1e1f
Sofia Godovykh <sofia-nikolaenko@yandex.ru>
2014-07-30 16:08:03 +0700

    fix serializing

--- Serializer.m
+++ Serializer.m
@@ -14,12 +14,11 @@
     if(![dictionary isKindOfClass:[NSDictionary class]])
         return NO; //TODO: error
     
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"strart to reading dictionary \n"];
-    for (id key in dictionary)
-    {
-        [reading appendString:[Serializer switching:[dictionary objectForKey:key]]];
-    }
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n"];
+    [reading appendString: [Serializer switching:dictionary]];
     
+   
+
     return reading;
 }
 
@@ -57,61 +56,90 @@
 
 +(NSString*)serializeDictionary:(NSDictionary*) dictionary
 {
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n this item is a dictionary \n"];
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n{"];
     for (id key in dictionary)
     {
+        [reading appendString:@"\n    \""];
+        [reading appendString:key];
+        [reading appendString:@"\":"];
         [reading appendString:[Serializer switching:[dictionary objectForKey:key]]];
     }
     
+    NSRange range;
+    range.location = reading.length - 2;
+    range.length = 1;
+    [reading deleteCharactersInRange:range];
+    [reading appendString:@"\n}"];
     return reading;
 
 }
 
 +(NSString*)serializeSet:(NSSet*) set
 {
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n this item is a set \n"];
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"["];
     for (id item in set)
     {
+        [reading appendString:@"\n"];
         [reading appendString:[Serializer switching:item]];
+        [reading appendString:@", "];
     }
+    NSRange range;
+    range.location = reading.length - 2;
+    range.length = 1;
+    [reading deleteCharactersInRange:range];
     
+    [reading appendString:@"]"];
     return reading;
 }
 
 +(NSString*)serializeArray:(NSArray*) array
 {
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n this item is an array \n"];
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"["];
     for (id item in array)
     {
+        [reading appendString:@"\n"];
         [reading appendString:[Serializer switching:item]];
+        [reading appendString:@", "];
     }
     
+    NSRange range;
+    range.location = reading.length - 3;
+    range.length = 2;
+    [reading deleteCharactersInRange:range];
+
+    [reading appendString:@"]"];
     return reading;
 }
 
 +(NSString*)serializeNull:(NSNull*) nullValue
 {
-    return @" null";
+    return @"null";
 }
 
 +(NSString*)serializeNumber:(NSNumber*) number
 {
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@" "];
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@""];
     NSMutableString *str = [[NSMutableString alloc] init];
     [str appendFormat: @"%s", [number objCType]];
     if([str  isEqual: @"c"])
     {
         if([[number stringValue]  isEqual: @"1"])
         {
+            [reading appendString:@"\""];
             [reading appendFormat: @"%@", @"YES"];
+            [reading appendString:@"\""];
             return reading;
         }
         else if ([[number stringValue]  isEqual: @"0"])
         {
+            [reading appendString:@"\""];
             [reading appendFormat: @"%@", @"NO"];
+            [reading appendString:@"\""];
             return reading;
         }
+        [reading appendString:@"\""];
         [reading appendFormat: @"%c", [number charValue]];
+        [reading appendString:@"\""];
         return reading;
     }
     
@@ -121,17 +149,25 @@
 
 +(NSString*)serializeNSValue:(NSValue*) value
 {
-    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n This is a CGRect \n"];
+    NSMutableString *reading = [[NSMutableString alloc] initWithString:@"\n{\n"];
     CGRect rect;
     [value getValue:&rect];
-    [reading appendFormat: @"%s", " X "];
+    
+    [reading appendString: @"\"origin.x\": \""];
     [reading appendFormat: @"%f", rect.origin.x];
-    [reading appendFormat: @"%s", " Y "];
+    [reading appendString: @"\"\n"];
+    
+    [reading appendString: @"\"origin.y\": \""];
     [reading appendFormat: @"%f", rect.origin.y];
-    [reading appendFormat: @"%s", " height "];
+    [reading appendString: @"\"\n"];
+    
+    [reading appendString: @"\"size.height\": \""];
     [reading appendFormat: @"%f", rect.size.height];
-    [reading appendFormat: @"%s", " width "];
+    [reading appendString: @"\"\n"];
+    
+    [reading appendString: @"\"size.width\": \""];
     [reading appendFormat: @"%f", rect.size.width];
+    [reading appendString: @"\"\n}"];
     return reading;
 }
 @end
