ce86ef73c6f5e823f8ad410cb00c6797a8199b63
Saveliy Leontiev <sava0796@mail.ru>
2016-07-11 13:38:50 +0600

    Initial commit

---
+++ Serializer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Serializer
+//
+//  Created by admin on 11/07/16.
+//  Copyright © 2016 Saveliy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
